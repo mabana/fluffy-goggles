ad65df4553208077f22333137629604848e3e187
Marcin Banasiak <mnbnsk@gmail.com>
2017-08-06 12:45:01 +0200

    Add static files server

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/julienschmidt/httprouter"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Game")
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+	return (":" + port)
+}
+
 func main() {
-	fmt.Println("Game")
+	router := httprouter.New()
+
+	router.GET("/", Index)
+	router.ServeFiles("/assets/*filepath", http.Dir("public"))
+
+	log.Fatal(http.ListenAndServe(getPort(), router))
 }
