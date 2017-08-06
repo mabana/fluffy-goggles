4f75d2e32eae051556ad32ca9e1f87b02de09a95
Marcin Banasiak <mnbnsk@gmail.com>
2017-08-06 18:19:38 +0200

    Add webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: "./src/main.tsx",
+    output: {
+        filename: "game.js",
+        path: __dirname + "../public/"
+    },
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    }
+};
